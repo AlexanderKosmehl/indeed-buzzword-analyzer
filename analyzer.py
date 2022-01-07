bfd1ba08911f30b7bf8099ae66551be04a285e8d
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2022-01-07 18:34:06 +0100

    Converted analyzer into functions to be used in jupyter notebook

--- analyzer.py
+++ analyzer.py
@@ -2,26 +2,15 @@ import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
 
-# Buzzwords
-buzzwords = ["HTML", "Python", "Java", "Kotlin", "Office", "Excel"]
-
-# Captcha Workaround:
-# Replace de.indeed.com -> indeed.com and Koeln -> New York, NY
-
-# Query Terms
-title = "Android"
-location = "Köln"
-pages = 1 # 15 listings per page
-
-# Get necessary parameters for pages
-base_url = "https://de.indeed.com"
-url = f"{base_url}/jobs?q={title}&l={location}"
-
 
 def getPageSoup(page_url):
+    """Looks up the specified page and parses the html content into BeautifulSoup."""
+
+    # Somewhat convincing header information
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62",
-        'referer': 'https://www.google.com/'}
+        'referer': 'https://www.google.com/'
+    }
 
     page = requests.get(page_url, headers)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -29,62 +18,86 @@ def getPageSoup(page_url):
     return soup
 
 
-job_urls = []
+def getJobDescriptions(title, location, num_pages=1):
+    """
+    Looks up the first num_pages x 15 job descriptions and returns them as a list.
+    
+    Parameters
+    ----------
 
-# Iterate through the first 4 pages (15 job listings per page)
-for page_index in range(0, pages):
-    # Prepare paged url
-    starting_index = page_index * 10
-    page_url = url + f"&start={starting_index}"
+    title: str
+        The job title used to search LinkedIn
+    location: str
+        The location used to search LinkedIn
+    num_pages: int, optional
+        Number of pages to get job descriptions from. Each page contains 15 descriptions.
+    """
 
-    # Parse Page
-    soup = getPageSoup(page_url)
+    # Captcha Workaround:
+    # Replace de.indeed.com -> indeed.com and Koeln -> New York, NY
+    base_url = "https://de.indeed.com"
+    url = f"{base_url}/jobs?q={title}&l={location}"
 
-    # Add links to list
-    cards = soup.find_all("a", class_="resultWithShelf")
-    links = [card["href"] for card in cards]
+    job_urls = []
 
-    # Add all prepared links to list
-    for link in links:
-        job_urls.append(base_url + link)
+    # Iterate through the first 4 pages (15 job listings per page)
+    for page_index in range(0, num_pages):
+        # Prepare paged url
+        starting_index = page_index * 10
+        page_url = url + f"&start={starting_index}"
+
+        # Parse Page
+        soup = getPageSoup(page_url)
+
+        # Add links to list
+        cards = soup.find_all("a", class_="resultWithShelf")
+        links = [card["href"] for card in cards]
+
+        # Add all prepared links to list
+        for link in links:
+            job_urls.append(base_url + link)
+
+    if len(job_urls) == 0:
+        raise Exception("No Urls found!")
+
+    job_descriptions = []
+
+    # Iterate through all job listings
+    for idx, job_url in enumerate(job_urls):
+        print(f"{idx + 1} / {len(job_urls)}")
+
+        # Get a single page
+        job_soup = getPageSoup(job_url)
+
+        # Find the job description block
+        job_description_soup = job_soup.find("div", id="jobDescriptionText")
+
+        # Generate single string
+        joined_strings = " ".join(job_description_soup.strings)
+
+        # Add string to list
+        job_descriptions.append(joined_strings)
+
+    return job_descriptions
 
 
-if len(job_urls) == 0:
-    raise Exception("No Urls found!")
+def getBuzzwordCounts(job_descriptions, buzzwords):
+    """
+    Goes through the job_descriptions and checks for buzzword occurences.
+    
+    Parameters
+    ----------
 
-job_descriptions = []
+    job_description: list<string>
+        List of job_descriptions to look through.
+    buzzwords: list<string>
+        List of buzzwords to look for.
+    """
+    buzzword_counts = {buzzword: 0 for buzzword in buzzwords}
 
-# Iterate through all job listings
-for idx, job_url in enumerate(job_urls):
-    print(f"{idx + 1} / {len(job_urls)}")
+    for job_description in job_descriptions:
+        for buzzword in buzzwords:
+            if buzzword in job_description:
+                buzzword_counts[buzzword] += 1
 
-    # Get a single page
-    job_soup = getPageSoup(job_url)
-
-    # Find the job description block
-    job_description_soup = job_soup.find("div", id="jobDescriptionText")
-
-    # Generate single string
-    joined_strings = " ".join(job_description_soup.strings)
-
-    # Add string to list
-    job_descriptions.append(joined_strings)
-
-
-buzzword_counts = {buzzword: 0 for buzzword in buzzwords}
-
-# Check Job Descriptions for Buzzwords
-for job_description in job_descriptions:
-    for buzzword in buzzwords:
-        if buzzword in job_description:
-            buzzword_counts[buzzword] += 1
-
-# Sort by counts
-sorted_buzzwords = {k: v for k, v in sorted(buzzword_counts.items(), key=lambda x: x[1], reverse=True)}
-
-# Plots the results
-plt.title("Buzzword Analysis")
-plt.xlabel("Buzzword")
-plt.ylabel("Occurences")
-plt.bar(sorted_buzzwords.keys(), sorted_buzzwords.values())
-plt.show()
+    return buzzword_counts
